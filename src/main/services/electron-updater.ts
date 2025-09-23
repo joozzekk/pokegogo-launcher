61041a38ae385da2ffe746d2851742e57ed96434
Dominik Jóźwicki <mr.jozvvicki@gmail.com>
2025-09-23 21:01:34 +0200

    Added autoupdater logging

--- src/main/services/electron-updater.ts
+++ src/main/services/electron-updater.ts
@@ -1,6 +1,13 @@
 import electronUpdater, { type AppUpdater } from 'electron-updater'
+import * as log from 'electron-log'
 
 export function getAutoUpdater(): AppUpdater {
   const { autoUpdater } = electronUpdater
+
+  autoUpdater.logger = log
+  log.transports.file.level = 'info'
+
+  autoUpdater.forceDevUpdateConfig = true
+
   return autoUpdater
 }
