ddcad3976775ce492377feed2f7a6c76b5e91c51
Dominik Jóźwicki <mr.jozvvicki@gmail.com>
2025-10-10 21:38:40 +0200

    Fixed second instance

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,31 +3,51 @@ import { app, BrowserWindow } from 'electron'
 import { electronApp, optimizer } from '@electron-toolkit/utils'
 import useWindowService from './services/window-service'
 import { useAppUpdater } from './services/app-updater'
+import { createTray } from './services/tray-service'
 
-app.whenReady().then(async () => {
-  electronApp.setAppUserModelId('pl.pokemongogo')
+const gotTheLock = app.requestSingleInstanceLock()
 
-  const { createMainWindow, createLoadingWindow } = useWindowService()
-  const mainWindow = createMainWindow()
-  const { startApp } = createLoadingWindow()
-  const appUpdater = useAppUpdater(mainWindow)
+if (!gotTheLock) {
+  app.quit()
+} else {
+  let mainWindow: BrowserWindow | null = null
 
-  await startApp(appUpdater, mainWindow)
-  await installExtension(VUEJS_DEVTOOLS)
+  app.on('second-instance', () => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.show()
+      mainWindow.focus()
+    }
+  })
 
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
+  app.whenReady().then(async () => {
+    electronApp.setAppUserModelId('pl.pokemongogo')
+
+    const { createMainWindow, createLoadingWindow } = useWindowService()
+    mainWindow = createMainWindow()
+    const { startApp } = createLoadingWindow()
+    const appUpdater = useAppUpdater(mainWindow)
+
+    createTray(mainWindow)
+    await startApp(appUpdater, mainWindow)
+    await installExtension(VUEJS_DEVTOOLS)
+
+    app.on('browser-window-created', (_, window) => {
+      optimizer.watchWindowShortcuts(window)
+    })
   })
 
   app.on('activate', async () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      await startApp(appUpdater, mainWindow)
+      if (mainWindow) {
+        await mainWindow.show()
+      }
     }
   })
-})
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+}
